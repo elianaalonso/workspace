b5c4daccaab6d1506359c0f90d55d957c42446a5
PilarRodriguez27 <rodriguezpilar037@gmail.com>
2024-08-22 19:38:17 -0300

    Pronto

--- js/login.js
+++ js/login.js
@@ -1,24 +1,3 @@
-document.getElementById("login-form").addEventListener("submit", function (event) {
-    event.preventDefault();  // Prevenir el comportamiento por defecto del formulario (recarga de la página)
-
-    // Obtener valores de los campos
-    let usuario = document.getElementById("usuario").value;
-    let contraseña = document.getElementById("contraseña").value;
-
-    // Validar que ambos campos no estén vacíos
-    if (usuario === "" || contraseña === "") {
-        alert("Por favor, completa todos los campos.");
-    } else {
-        // Redireccionar a la página de portada 
-        window.location.href = "index.html";
-    }
-});
-
-
-
-
-
-
 /*Pilar*/
 
 sessionStorage.setItem("isLoggedIn", "true");
