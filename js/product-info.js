77bf43f656e3bec4c8f5cb95ecc9ea281c4acd18
camilacuitino23 <camilacuitino23@gmail.com>
2024-09-12 16:04:49 -0300

    product info's fetch

--- js/product-info.js
+++ js/product-info.js
@@ -6,3 +6,36 @@ function checkLogin() {
   }
   
   window.onload = checkLogin;
+
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    const productId = localStorage.getItem('selectedProductId');
+    
+    if (productId) {
+        const apiUrl = `https://japceibal.github.io/emercado-api/products/${productId}.json`;
+
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(product => {
+                document.getElementById('product-name').textContent = `${product.name}`;
+                document.getElementById('product-description').textContent = `${product.description}`;
+                document.getElementById('product-cost').textContent = `${product.currency} ${product.cost}`;
+                document.getElementById('product-category').textContent = `${product.category}`;
+                document.getElementById('product-sold-count').textContent = `${product.soldCount} vendidos`;
+
+                const imagesContainer = document.getElementById('product-images');
+                product.images.forEach(imageUrl => {
+                    const img = document.createElement('img');
+                    img.src = imageUrl;
+                    img.alt = product.name;
+                    imagesContainer.appendChild(img);
+                });
+
+            })
+            .catch(error => {
+                console.error('Error fetching product data:', error);
+            });
+    } else {
+        console.error('No product ID found in localStorage.');
+    }
+});
